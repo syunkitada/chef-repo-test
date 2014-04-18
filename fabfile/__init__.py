cae596112c2954e06e0093b4291b65fd275f67e0
syunkitada <syun.kitada@gmail.com>
2014-04-19 08:25:54 +0900

    updated

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,71 +1,116 @@
 # coding: utf-8
 from fabric.api import *
 import re, os, json, commands
+import conf
+import datetime
 
 env.forward_agent = True
 
-PWD           = os.environ['PWD']
-PATH_TO_NODES = PWD + '/nodes'
-PATH_TO_ROLES = PWD + '/roles'
+def complement_path(path):
+    if path.find('/') == 0:
+        return path
+    else:
+        return os.path.join(os.path.dirname(__file__), path)
 
+chef_repo_path = complement_path(conf.chef_repo_path)
+nodes_path     = complement_path(conf.nodes_path)
+role_path      = complement_path(conf.roles_path)
+chef_rpm_path  = complement_path(conf.chef_rpm_path)
+
+
+@task
 def node(reg_host='*'):
-	hosts = __get_hosts(reg_host)
-	host = 'HostName'
-	uptime = 'Uptime'
-	last_cook = 'LastCook'
-	run_list = 'RunList'
-	print '%(host)-25s%(uptime)-15s%(last_cook)-25s%(run_list)s' % locals()
-	print '----------------------------------------------------------------------------'
-	for host in hosts:
-		path = '%s/%s.json' % (PATH_TO_NODES, host)
-		with open(path, 'r') as f:
-			host_json = json.load(f)
-		ip_address = host_json.get('ip_address')
-		uptime = host_json.get('uptime', '')
-		prog = re.compile('^.*up (.+),.*user.*$')
-		progs = prog.search(uptime)
-		if progs:
-			uptime = progs.group(1)
-		last_cook = host_json.get('last_cook')
-		run_list = host_json.get('run_list')
-		print '%(host)-25s%(uptime)-15s%(last_cook)-25s%(run_list)s' % locals()
+    hosts = __get_hosts(reg_host)
+    host = 'HostName'
+    uptime = 'Uptime'
+    last_cook = 'LastCook'
+    run_list = 'RunList'
+    print '%(host)-30s%(uptime)-15s%(last_cook)-25s%(run_list)s' % locals()
+    print '----------------------------------------------------------------------------'
+    for host in hosts:
+        path = '%s/%s.json' % (nodes_path, host)
+        with open(path, 'r') as f:
+            host_json = json.load(f)
+        ip_address = host_json.get('ip_address')
+        uptime = host_json.get('uptime', '')
+        prog = re.compile('^.*up (.+),.*user.*$')
+        progs = prog.search(uptime)
+        if progs:
+            uptime = progs.group(1)
+        last_cook = host_json.get('last_cook')
+        run_list = host_json.get('run_list')
+        print '%(host)-30s%(uptime)-15s%(last_cook)-25s%(run_list)s' % locals()
 
+@task
 def role(reg_role='*'):
-	roles = commands.getoutput('find %s/*.rb' % (PATH_TO_ROLES))
-	print roles
+    roles = commands.getoutput('find %s/*.rb' % (roles_path))
+    print roles
 
-def cook():
-	with shell_env(PASSWORD=env.password):
-		local('knife solo cook %s --ssh-password $PASSWORD' % (env.host))
+@task
+def cook(option=None):
+    if option and option == 'p' \
+            and hasattr(conf, 'http_proxy') and http_proxy != '' \
+            and hasattr(conf, 'https_proxy') and http_proxy != '':
+        with shell_env(PASSWORD=env.password,
+                http_proxy=conf.http_proxy, https_proxy=conf.http_proxy):
+            local('knife solo cook %s --sync-only --ssh-password $PASSWORD' % (env.host))
+            sudo('chef-solo -c chef-solo/solo.rb -j chef-solo/dna.json')
+    else:
+        with shell_env(PASSWORD=env.password):
+            local('knife solo cook %s --ssh-password $PASSWORD' % (env.host))
 
-	path = '%s/%s.json' % (PATH_TO_NODES, env.host)
-	with open(path, 'r') as f:
-		host_json = json.load(f)
+    path = '%s/%s.json' % (nodes_path, env.host)
+    with open(path, 'r') as f:
+        host_json = json.load(f)
 
-	date = run('date +"%Y-%m-%d %I:%M:%S"')
-	host_json.update({'last_cook': date})
+    date = run('date +"%Y-%m-%d %I:%M:%S"')
+    host_json.update({'last_cook': date})
 
-	uptime = run('uptime')
-	host_json.update({'uptime': uptime})
+    uptime = run('uptime')
+    host_json.update({'uptime': uptime})
 
-	with open(path, 'w') as f:
-		json.dump(host_json, f)
+    with open(path, 'w') as f:
+        json.dump(host_json, f)
 
+@task
 def prepare():
-	with shell_env(PASSWORD=env.password):
-		local('knife solo prepare %s --ssh-password $PASSWORD' % (env.host))
+    if hasattr(conf, 'chef_rpm_path') and chef_rpm_path != '':
+        chef_rpm = 'tmp_chef.rpm'
+        if os.path.exists(chef_rpm_path):
+            local('scp %s %s:~/%s' % (chef_rpm_path, env.host, chef_rpm))
+            with settings(ok_ret_codes=[0,1]):
+                sudo('yum install %s -y' % chef_rpm)
+                run('rm -rf %s' % chef_rpm)
+        else:
+            print 'cannot access %s' % chef_rpm_path
+    else:
+        with shell_env(PASSWORD=env.password):
+            local('knife solo prepare %s --ssh-password $PASSWORD' % (env.host))
+
+    path = '%s/%s.json' % (nodes_path, env.host)
+    with open(path, 'r') as f:
+        host_json = json.load(f)
+
+    host_json.update({'last_cook': 'prepared'})
+
+    uptime = run('uptime')
+    host_json.update({'uptime': uptime})
+
+    with open(path, 'w') as f:
+        json.dump(host_json, f)
 
 # hostsの設定と、sudo passwordの設定を行う
 # cook prepareをする場合は必ず実行
+@task
 @hosts('localhost')
 def h(*xargs):
-	sudo('hostname')
-	env.hosts = __get_hosts(xargs)
+    sudo('hostname')
+    env.hosts = __get_hosts(xargs)
 
 def __get_hosts(xargs):
-	hosts = set()
-	prog = re.compile('%s/(.*).json' % PATH_TO_NODES)
-	for reg_host in xargs:
-		host_jsons = commands.getoutput('find %s/ -name %s.json' % (PATH_TO_NODES, reg_host))
-		hosts.update(set(prog.findall(host_jsons)))
-	return hosts
+    hosts = set()
+    prog = re.compile('%s/(.*).json' % nodes_path)
+    for reg_host in xargs:
+        host_jsons = commands.getoutput('find %s/ -name %s.json' % (nodes_path, reg_host))
+        hosts.update(set(prog.findall(host_jsons)))
+    return hosts
---
+++ fabfile/conf.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+chef_repo_path = '../'
+nodes_path     = '../nodes'
+roles_path     = '../roles'
+chef_rpm_path  = '/etc/chef/test.rpm'
+http_proxy     = ''
+https_proxy    = ''
